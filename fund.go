5afdf6a23a49a32cc3b9f73713d44d91e150af18
karelmalan <karelmalan@gmail.com>
2016-05-21 16:38:25 +0200

    Update fund.go
    copy-pasted source from tutorial page

--- fund.go
+++ fund.go
@@ -1,10 +1,24 @@
-// fund.go
-package main
+package funding
 
-import (
-	"fmt"
-)
+type Fund struct {
+    // balance is unexported (private), because it's lowercase
+    balance int
+}
 
-func main() {
-	fmt.Println("toptal example...")
+// A regular function returning a pointer to a fund
+func NewFund(initialBalance int) *Fund {
+    // We can return a pointer to a new struct without worrying about
+    // whether it's on the stack or heap: Go figures that out for us.
+    return &Fund{
+        balance: initialBalance,
+    }
+}
+
+// Methods start with a *receiver*, in this case a Fund pointer
+func (f *Fund) Balance() int {
+    return f.balance
+}
+
+func (f *Fund) Withdraw(amount int) {
+    f.balance -= amount
 }
